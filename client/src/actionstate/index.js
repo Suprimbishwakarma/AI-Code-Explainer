34c884e4972b6319b6e535f1c9470193c9d9ef6c
suprimbishwakarma <suprimbishwakarma2002@gmail.com>
2025-12-07 20:06:22 +0545

    comment added

--- client/src/actionstate/index.js
+++ client/src/actionstate/index.js
@@ -1,33 +1,35 @@
 "use server";
 
 export async function codeExplain(prevState, formData) {
+  //Get the code and language from the form.
   const code = formData.get("code");
   const language = formData.get("language");
 
   try {
+    //send a request to the server.
     const result = await fetch(`${import.meta.env.VITE_SERVER_URL}`, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({code, language}),
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ code, language }),
     });
 
-    if(!result.ok){
-        return {
-            success: false,
-            error: "failed to fetch the result!"
-        }
+    if (!result.ok) {
+      return {
+        success: false,
+        error: "failed to fetch the result!",
+      };
     }
 
     const data = await result.json();
 
     return {
-        success: true,
-        data
-    }
+      success: true,
+      data,
+    };
   } catch (error) {
-    return{
-        success: false,
-        error: `An error occured: ${error.message}`
-    }
+    return {
+      success: false,
+      error: `An error occured: ${error.message}`,
+    };
   }
 }
