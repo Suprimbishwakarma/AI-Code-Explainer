1a1e516f2f724f266bd53f3a86227f2c87253e17
suprimbishwakarma <suprimbishwakarma2002@gmail.com>
2026-01-08 09:36:01 +0545

    deployment issue fixes

--- server/app.js
+++ server/app.js
@@ -11,8 +11,10 @@ app.use(express.json());
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "*",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
